2891c5bce68faac3fd8d24efd747d37e63019149
Jeri Fey <jericfey@gmail.com>
2020-11-01 15:34:15 -0700

    getting input to localStorage added

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,11 @@
+$(document).ready(function () {
+  //FUNCTIONS
 
-$(document).ready(function() {
+  var storeCity = function () {
+    localStorage.setItem("cityInput", JSON.stringify());
+  };
 
-    //FUNCTIONS
-    
-
-
-
-
-
-
-
-
-    // API call for OpenWeather
-    api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3976a448c3612fdae22654b75c5eca9a
-    
-    //5 day forcast API
-    api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
-
-
-
-
-
-
-
-
-
-
-    //EVENT LISTENERS
-
-
-})
-
-
+  //EVENT LISTENERS
+  //Event listener for city input on click
+  $("#search").on("click", storeCity);
+});
