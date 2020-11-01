19a31cbe0acaa3d37c12c55918e815a5084a89ca
Jeri Fey <jericfey@gmail.com>
2020-11-01 15:47:56 -0700

    Trying another option for local Storage

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,21 @@ $(document).ready(function () {
   //FUNCTIONS
 
   var storeCity = function () {
-    localStorage.setItem("cityInput", JSON.stringify());
+    //get input info
+    var cityInput = $("#city-input").val();
+    //Store data
+    localStorage.setItem("city", cityInput);
+    alert("The city name has been saved.");
   };
+
+  // var callCity = function () {
+  //   //call city from local storage
+  //   alert("You selected: " + localStorage.getItem(cityInput));
+  // };
 
   //EVENT LISTENERS
   //Event listener for city input on click
-  $("#search").on("click", storeCity);
+  $("#search").on("click", storeCity());
+  //Call cityname from storage when city button is clicked
+  // $("#city-button").on("click", callCity);
 });
