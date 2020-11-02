1551d012baffd90cbc745be7e095d040bcf1d43e
Jeri Fey <jericfey@gmail.com>
2020-11-01 17:11:20 -0700

    Local Storage and button generation working

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,9 @@ $(document).ready(function () {
     // console.log("The city name has been saved." + cityInput);
     //generate button with city name
     $("#city-list").append(
-      '<button class="list-group-item list-group-item-action bg-light" id=""></button>'
+      `<button class="list-group-item list-group-item-action bg-light" id="${cityInput}">${cityInput}</button>`
     );
-    $(this).prop("id", cityInput);
-    $(this).prop("value", cityInput);
+
     // $("#city-list").append(
     //   $(document.createElement("button")).prop({
     //     type: "button",
