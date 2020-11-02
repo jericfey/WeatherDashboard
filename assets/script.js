230bb52db0d68ecaac75a1c5efee5ea40b13a1dc
Jeri Fey <jericfey@gmail.com>
2020-11-01 21:52:08 -0700

    City input and weather card are working

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     // console.log("The city name has been saved." + cityInput);
     //generate button with city name
     $("#city-list").append(
-      `<button class="list-group-item list-group-item-action bg-light" id="${cityInput}">${cityInput}</button>`
+      `<button class="list-group-item list-group-item-action bg-light" id="city-name" value="${cityInput}">${cityInput}</button>`
     );
     //Clear input field
     $("#city-input").val("");
@@ -24,9 +24,45 @@ $(document).ready(function () {
   var displayWeather = function () {
     //show cards once city has been entered
     $("#weather-cards").show();
+    //clear div before start
+    // $("#city-title").empty();
+    $("#temp").empty();
+
+    //Trying to call value from button pushed
+    // alert($(this).attr("value"));
 
     //call city from local storage
     var cityInput = localStorage.getItem("city");
+
+    //Store lat/lon coordinates for selected city to local storage
+    var requestUrlLatLon = "http://ip-api.com/json/?city=" + cityInput;
+    $.ajax({
+      url: requestUrlLatLon,
+      method: "GET",
+    }).then(function (data) {
+      // console.log(lat);
+      // console.log(lon);
+      localStorage.setItem("lat", data.lat);
+      localStorage.setItem("lon", data.lon);
+    });
+
+    //Get UV index and save to local storage
+    var lat = localStorage.getItem("lat");
+    var lon = localStorage.getItem("lon");
+    var requestUrlUv =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=3976a448c3612fdae22654b75c5eca9a&lat=" +
+      lat +
+      "&lon=" +
+      lon;
+    // console.log(requestUrlUv);
+    $.ajax({
+      url: requestUrlUv,
+      method: "GET",
+    }).then(function (data) {
+      // console.log(data.value);
+      localStorage.setItem("uv", data.value);
+    });
+
     //Add city input into API request url
     var requestURL =
       "https://api.openweathermap.org/data/2.5/weather?appid=3976a448c3612fdae22654b75c5eca9a&q=" +
@@ -60,12 +96,17 @@ $(document).ready(function () {
       var cityName = $("<span>");
       var weatherImage = $("<img>");
       var weatherDesc = $("<span>");
-
-      //temperature, humidity, wind speed, UV index, city name, today's date
+      var weatherCard = $("#temp");
+      var temp = $("<span>");
+      var temperature = data.main.temp;
+      var humidity = data.main.humidity;
+      var wind = data.wind.speed;
+      var uv = localStorage.getItem("uv");
 
       //get data into element created above
       cityName.text(data.name);
       weatherDesc.text(data.weather[0].description);
+      // temp.text(data.main.temp);
 
       //modify image element attributes
       weatherImage.attr("src", iconURL);
@@ -75,6 +116,10 @@ $(document).ready(function () {
       cityNameDiv.append("  " + currentDate);
       cityNameDiv.append(weatherImage);
       cityNameDiv.append(weatherDesc);
+      weatherCard.append(`Temperature: ${temperature} &deg;F <br>`);
+      weatherCard.append(`Humidity: ${humidity} % <br>`);
+      weatherCard.append(`Wind Speed: ${wind} MPH <br>`);
+      weatherCard.append(`UV Index: ${uv}`);
 
       // $("#city-title").css("display", "inline-block");
     });
@@ -83,4 +128,5 @@ $(document).ready(function () {
   //EVENT LISTENERS
   //Event listener for city input on click
   $("#search").on("click", newCity);
+  // $("#city-name").on("click", displayWeather);
 });
